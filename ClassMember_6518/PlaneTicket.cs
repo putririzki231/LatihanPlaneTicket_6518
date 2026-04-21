6ef591cf56769648800a3e30d14e299b70a3c64d
putririzki231 <putririzki231@gmail.com>
2026-04-21 19:42:54 +0700

    Add files via upload

---
+++ ClassMember_6518/PlaneTicket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMember_6518
+{
+    internal class PlaneTicket
+    {
+        
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Cost { get; set; }
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine($"Origin: {Origin}");
+            Console.WriteLine($"Destination: {Destination}");
+            Console.WriteLine($"Cost: {Cost} ({Currency})");
+
+            if (Currency.ToUpper() == "USD")
+            {
+                double convertToRupiah = Cost * 15000;
+                Console.WriteLine($"Convert to Rupiah: {convertToRupiah}");
+            }
+            else if (Currency.ToUpper() == "IDR")
+            {
+                double convertToDollar = Cost / 15000;
+                Console.WriteLine($"Convert to Dollar: {convertToDollar}");
+            }
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
---
+++ ClassMember_6518/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMember_6518
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            PlaneTicket ticket2 = new PlaneTicket();
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+
+            Console.WriteLine("Tekan sembarang tombol untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
